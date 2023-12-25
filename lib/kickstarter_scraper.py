2a9e88ce032167525f01244f4cf86cd9dde267dc
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-26 01:34:16 +0300

    added print statements to display the output, corrected attribute access method in BeautifulSoup parsing

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -1,3 +1,4 @@
+# file: kickstarter_scraper.py
 from bs4 import BeautifulSoup
 import ipdb
 
@@ -8,11 +9,34 @@ import ipdb
 # location: project.select("ul.project-meta span.location-name")[0].text
 # percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
 
-
-html = ''
 def create_project_dict():
-    with open('../fixtures/kickstarter.html') as file:
+  html = ''
+  with open('../fixtures/kickstarter.html') as file:
         html = file.read()
-    kickstarter = BeautifulSoup(html, 'html.parser')
-    ipdb.set_trace()
-create_project_dict()
+
+  kickstarter = BeautifulSoup(html, 'html.parser')
+  projects = {}
+  # Iterate through the projects
+  for project in kickstarter.select("li.project.grid_4"):
+    # Get the first element
+    title = project.select("h2.bbcard_name strong a")[0].text
+
+
+    projects[title] = {
+       'image_link': project.select("div.project-thumbnail a img")[0].get("src"),
+       'description': project.select("p.bbcard_blurb")[0].text,
+       'location': project.select("ul.project-meta span.location-name")[0].text,
+       'percent_funded': project.select("ul.project-stats li.first.funded strong")
+    [0].text.replace("%","")
+    }
+
+  # return the projects dictionary
+  return projects
+
+if __name__ == "__main__":
+  # Call the function and assign the result to a variable
+  projects_dict = create_project_dict()
+  # Print the variable
+  print(projects_dict)
+
+
